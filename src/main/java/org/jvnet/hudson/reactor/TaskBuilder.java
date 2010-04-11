0c1889ead4e1b8db488a1e9e90f4174c99229bfa
kohsuke <kohsuke@@java.net>
2010-04-11 16:07:05 +0000

    doc improvement and added some more convenience methods.

--- src/main/java/org/jvnet/hudson/reactor/TaskBuilder.java
+++ src/main/java/org/jvnet/hudson/reactor/TaskBuilder.java
@@ -1,12 +1,24 @@
 package org.jvnet.hudson.reactor;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 /**
+ * Contributes {@link Task}s to a {@link Reactor} execution.
+ *
  * @author Kohsuke Kawaguchi
  */
 public abstract class TaskBuilder {
+    /**
+     * Returns all the tasks that this builder contributes to.
+     */
     public abstract Iterable<? extends Task> discoverTasks(Reactor reactor) throws IOException;
 
     /**
@@ -19,4 +31,25 @@ public abstract class TaskBuilder {
             }
         };
     }
+
+    public static TaskBuilder union(final Iterable<? extends TaskBuilder> builders) {
+        return new TaskBuilder() {
+            public Iterable<? extends Task> discoverTasks(Reactor reactor) throws IOException {
+                List<Task> r = new ArrayList<Task>();
+                for (TaskBuilder b : builders)
+                    for (Task t : b.discoverTasks(reactor))
+                        r.add(t);
+                return r;
+            }
+        };
+    }
+
+    public static TaskBuilder union(TaskBuilder... builders) {
+        return union(asList(builders));
+    }
+
+    /**
+     * {@link TaskBuilder} that contributes no task.
+     */
+    public static final TaskBuilder EMPTY_BUILDER = fromTasks(Collections.<Task>emptyList());
 }
